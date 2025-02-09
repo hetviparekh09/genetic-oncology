aae1f99491e8a9aec306fa6b37eedbdebf847035
hetviparekh09 <parekhhetvi09@gmail.com>
2025-02-09 17:04:01 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ const Footer = () => {
   return (
     <footer>
       <p>© 2025 Genetic Oncology. All rights reserved.</p>
+      <p>This is a college project by Hetvi Parekh from D.Y. Patil University.</p>
     </footer>
   );
 };
