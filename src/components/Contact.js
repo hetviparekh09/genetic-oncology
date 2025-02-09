e8b457732e39a2325fb36366bc6e9997bb8817fd
hetviparekh09 <parekhhetvi09@gmail.com>
2025-02-09 17:10:44 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,16 +3,12 @@ import React from 'react';
 const Contact = () => {
   return (
     <div className="page">
-      <h1>Contact Us</h1>
-      <p>If you have any questions or inquiries, feel free to reach out to us.</p>
-      <form>
-        <label>Name:</label>
-        <input type="text" />
-        <label>Email:</label>
-        <input type="email" />
-        <textarea placeholder="Your message"></textarea>
-        <button type="submit">Submit</button>
-      </form>
+      <h1>Contact Information</h1>
+      <p>If you have any questions or inquiries, feel free to reach out to me:</p>
+      <ul>
+        <li>Email: <a href="mailto:het.par.bt22@dypatil.edu">het.par.bt22@dypatil.edu</a></li>
+        <li>Phone: <a href="tel:+919922999292">+91 99229 99292</a></li>
+      </ul>
     </div>
   );
 };
